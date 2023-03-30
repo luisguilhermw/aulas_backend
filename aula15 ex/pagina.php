ff57315562b966f973b5a05d2931ce77e9214f0e
LuÃ­s Guilherme de Souza Marques <14595766670@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-03-30 10:37:50 -0300

    Aula-16

--- aula15 ex/pagina.php
+++ aula15 ex/pagina.php
@@ -1,3 +1,22 @@
+<?php
+    
+    $cores[] = "yellow";
+    $cores[] = "green";
+    $cores[] = "blue";
+    $cores[] = "pink";
+    $cores[] = "red";
+    $cores[] = "purple";
+    $cores[] = "orange";
+    $cores[] = "grey";
+    $cores[] = "white";
+
+    if(isset($_GET['cor'])){
+        $cor = $_GET['cor'];
+      }else{
+        $cor = 'white';
+      }
+    
+?>
 
 <!DOCTYPE html>
 <html lang="en">
@@ -6,28 +25,22 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+<style>
+    body{
+        background-color: <?php echo $cor ?>;
+    }
+</style>
 </head>
 <body>
-    
-    <?php
-    $cor[] = "Yellow";
-    $cor[] = "Green";
-    $cor[] = "Blue";
-    $cor[] = "Pink";
-    $cor[] = "Red";
-    $cor[] = "Purple";
-    $cor[] = "Orenge";
-    $cor[] = "Grey";
-    $cor[] = "White";
 
-    foreach($cor as $Key => $cor){
-        echo "<a href = 'pagina.php?cor=$cor'>Mudar pra $cor</a>". "<br>";
-    }
+<?php
+        foreach($cores as $key => $cor){
+            echo "<a href='pagina.php?cor=$cor'>Mudar para $cor</a>"."<br>";
+        }
+    ?>
     
-  ?>
 </body>
 </html>
-
 
 
 
