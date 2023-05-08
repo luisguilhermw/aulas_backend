84989d38d6fc9ba6816c58a24af9548ea3c40602
LuÃ­s Guilherme de Souza Marques <14595766670@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-05-08 08:40:09 -0300

    Aula 18

---
+++ Aula 18/produto/atualizar.php
@@ -0,0 +1,32 @@
+<?php 
+
+require_once "../conexao.php";
+
+if(isset($_POST["nome"]) && isset($_POST["descricao"]) 
+        && isset($_POST["preco"]))
+{
+$nome = $_POST["nome"];
+$descricao = $_POST["descricao"];
+$preco = $_POST["preco"];
+$foto = "semfoto.png";
+
+
+//String com o comando SQL para ser executado no DB
+$sql = "INSERT INTO `produto` (`nome`, `descricao`, `preco`, `foto`) 
+VALUES (?, ?, ?, ?);";
+echo $sql;
+
+
+//Prepara o SQL para ser executado no banco de dados
+$comando = $conexao->prepare($sql);
+
+//adciona valores nos parâmetros 
+$comando->bind_param("ssds", $nome, $descricao, $preco, $foto);
+
+//executa o SQL - comando no bancos de dados
+$comando->execute();
+
+}
+//abre o arquivo form.php
+header("Location: form.php");
+
